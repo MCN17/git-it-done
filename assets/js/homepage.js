a5395d9abd37ce53d42ebea1c24db44f854f4d39
Marty <martynolan17@gmail.com>
2022-07-05 11:53:33 -0400

    made a fetch request to github api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,24 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos")
+// var getUserRepos = function () {
+//     var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+//         response.json().then(function(data) {
+//             console.log(data);
+//         })
+//     })
+//     console.log("outside");
+// };
+
+// getUserRepos();
+
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiURL).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+getUserRepos("mcn17");
