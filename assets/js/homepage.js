e7bfcd7296816700ca78019a7dd476f5a4ec3b9b
Marty <martynolan17@gmail.com>
2022-07-06 11:13:30 -0400

    added language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -11,6 +11,24 @@
 
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+                //console.log(response);
+            });
+            
+        } else {
+            alert("Error: Github User Not Found");
+        }
+    });
+};
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -111,5 +129,18 @@ function displayRepos(repos, searchTerm){
     //console.log(repos);
     // console.log(searchTerm);
 };
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+    //console.log(language);
+}
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 
 
